b9fa082e4d0fcf0ca68cfd18dd420e948a1c6fdb
AndyRivadeneyra <183406@ids.upchiapas.edu.mx>
2020-01-22 19:47:02 -0600

    2.0

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'diq79r2gir&l4l-@21rm=5v#ghwsc$2w_-d&(&@!ymkomh9ro@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
