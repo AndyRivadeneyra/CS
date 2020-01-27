6100d6fcb43704b1fca4096f9e72b3ac5bbf4147
AndyRivadeneyra <183406@ids.upchiapas.edu.mx>
2020-01-27 11:05:24 -0600

    second commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,8 +24,8 @@ SECRET_KEY = 'diq79r2gir&l4l-@21rm=5v#ghwsc$2w_-d&(&@!ymkomh9ro@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = ['*']
+    
+ALLOWED_HOSTS = ['localhost','35.173.238.73']
 
 
 # Application definition
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'rivadeneyradb',
+        'USER': 'postgres',
+        'PASSWORD': 'metamorfosis629',
+        'HOST': 'database-1.ca0zpjdujja4.us-east-1.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
